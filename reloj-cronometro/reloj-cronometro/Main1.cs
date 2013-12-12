72323d39dfc63dd0bb682cc23343672360b40247
dracule-mihawk <j_m_rg@hotmail.es>
2013-12-11 19:15:11 -0600

    reloj-cronometro

---
+++ reloj-cronometro/reloj-cronometro/Main1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Reloj_Crono
+{
+	class Reloj_Crono
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine("\r\nSeleccione una opcion:\r\n");
+			Console.WriteLine("1)Reloj \r\n2)Cronometro");
+			String opcion = Console.ReadLine();
+			int opc = int.Parse(opcion);
+			switch (opc) 
+			{
+				case 1:	Console.WriteLine("\rEntrando a modo reloj");
+			    		new Reloj();
+						break;			
+				case 2: Console.WriteLine("\rEntrando a modo cronometro");
+						new cronometro();
+						break;
+				default:Console.WriteLine("La opcion que Seleccione no Existe");
+						break;
+			}
+		}
+	}
+}
